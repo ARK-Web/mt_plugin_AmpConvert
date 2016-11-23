606e57f596a7166c04f2542e2e07eb6ea07de797
ohmura <ohmura@ark-web.jp>
2016-11-24 06:44:50 +0900

    refs #35636 プラグイン設定の文言修正

--- plugins/AmpConvert/lib/AmpConvert/L10N/ja.pm
+++ plugins/AmpConvert/lib/AmpConvert/L10N/ja.pm
@@ -7,5 +7,7 @@ use base 'AmpConvert::L10N::en_us';
 use vars qw( %Lexicon );
 
 %Lexicon = (
-    '_PLUGIN_DESCRIPTION' => 'AmpConvert',
+    '_PLUGIN_DESCRIPTION' => '<p>AMP対応支援ツールです。インラインStyleの除去や、imgタグ、YouTube埋め込みコードの ampタグ変換などを行います。</p>
+<a target="_blank" href="https://github.com/ARK-Web/mt_plugin_AmpConvert">プラグインホーム</a>',
 );
+1;
